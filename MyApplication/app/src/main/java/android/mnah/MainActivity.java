de5fef2361947bd8b8702600ab6fcc290f9f5d10
Michelle Karlsson <milk@itu.dk>
2020-01-30 14:19:00 +0100

    Initial setup

--- MyApplication/app/src/main/java/android/mnah/MainActivity.java
+++ MyApplication/app/src/main/java/android/mnah/MainActivity.java
@@ -1,14 +1,48 @@
 package android.mnah;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mStartButton;
+    private Button mAboutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+
+        mStartButton = findViewById(R.id.start_button);
+        mAboutButton = findViewById(R.id.about_button);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent captureIntent = new Intent(MainActivity.this, CaptureActivity.class);
+                startActivity(captureIntent);
+            }
+        });
+
+        mAboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                FragmentManager fm = getSupportFragmentManager();
+                Fragment fragment = fm.findFragmentById(R.id.container);
+                fragment = new AboutFragment();
+                fm.beginTransaction().add(R.id.container, fragment).addToBackStack("about").commit();
+
+            }
+        });
     }
+
+
 }
